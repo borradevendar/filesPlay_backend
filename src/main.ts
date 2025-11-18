d139232a6fdf5ff229e46f81d5479fe142d7a221
borradevendar <borradevendarsaiteja123@gmail.com>
2025-11-18 11:20:19 +0000

    railway -> vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: ['https://filesplayfrontend-production.up.railway.app', 'https://filesplaybackend-production.up.railway.app'],
+  origin: ['https://files-play-frontend-2yj8a6tlv-devendar-saiteja-borras-projects.vercel.app', 'https://filesplaybackend-production.up.railway.app'],
   credentials: true,
 });
   await app.listen(process.env.PORT || 3000);
